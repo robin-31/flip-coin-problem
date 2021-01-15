45b2345047ecdec433420e698d80cd06de16d75c
“robin-31” <rm.robinmanna01@gmail.com>
2021-01-15 10:56:47 +0530

    ADDED: loop through nad stroe into dict

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,10 +1,51 @@
 #!/bin/bash -x
 #hello to my flip coin problem
+read -p "enter how many times you want to flip a coin :" x
 isHead=1
-flipcoin=$((RANDOM%2))
-	if [ "$isHead" -eq "$flipcoin" ]
+declare -A toss
+counter=0
+for (( i=1; i<=x; i++ ))
+do
+	flipcoin=$((RANDOM%2))
+		if [ "$isHead" -eq "$flipcoin" ]
+		then
+			echo "it's head you are winner"
+			toss[$i]="H"
+		else
+			echo "it's tail you are losser"
+			toss[$i]="T"
+		fi
+done
+echo ${!toss[@]}
+echo ${toss[@]}
+headcounter=0
+tailcounter=0
+
+for i in ${toss[@]}
+do
+	if [ "$i" == "H" ]
 	then
-		echo "it's head you are winner"
+		((headcounter++))
 	else
-		echo "it's tail you are losser"
+		((tailcounter++))
 	fi
+done
+echo $headcounter
+echo $tailcounter
+
+headpercentage=$(( (headcounter*100)/x ))
+tailpercentage=$(( (tailcounter*100)/x ))
+
+echo $headpercentage
+echo $tailpercentage
+
+
+
+
+
+
+
+
+
+
+
