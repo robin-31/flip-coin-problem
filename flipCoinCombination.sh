c2bf0be392e270e37b607eee7a3f8a4cae3a6837
“robin-31” <rm.robinmanna01@gmail.com>
2021-01-15 10:29:43 +0530

    ADDED: random funtion to genrate head or tail

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,2 +1,10 @@
 #!/bin/bash -x
 #hello to my flip coin problem
+isHead=1
+flipcoin=$((RANDOM%2))
+	if [ "$isHead" -eq "$flipcoin" ]
+	then
+		echo "it's head you are winner"
+	else
+		echo "it's tail you are losser"
+	fi
